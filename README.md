dcdbea50f172117903a196e366093d6358b3fb30
sertdfyguhi-git <gdkamtalan@gmail.com>
2022-03-04 00:15:20 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # sertdfyguhi.github.io
-
-website for my account
-
 website: https://sertdfyguhi.github.io/
--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const gradients = [
-  ['#4CA1AF', '#C4E0E5'],
-  ['#4B79A1', '#283E51'],
-  ['#2980b9', '#2c3e50'],
-  ['#4DA0B0', '#D39D38'],
-  ['#1e3c72', '#2a5298'],
-  ['#6a3093', '#a044ff'],
-  ['#00C9FF', '#92FE9D'],
-  ['#f46b45', '#eea849'],
-  ['#fc00ff', '#00dbde'],
-  ['#2c3e50', '#3498db'],
-  ['#525252', '#3d72b4'],
-  ['#D1913C', '#FFD194'],
-  ['#136a8a', '#267871'],
-  ['#FEAC5E', '#4BC0C8'],
-  ['#d649d1', '#d69949'],
+// background gradient
+const rgb1 = [
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256)
 ]
 
-function gradient() {
-  const rn = Math.floor(Math.random() * gradients.length);
-  document.body.style.background = `linear-gradient(90deg, ${gradients[rn][0]}, ${gradients[rn][1]})`
+const rgb2 = [
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256)
+]
+
+document.body.style.background = `linear-gradient(90deg, rgb(${rgb1.join(', ')}), rgb(${rgb2.join(', ')}))`
+
+// typing
+const el = document.getElementById('name')
+const text = 'sertdfyguhi'
+let i = 0
+
+function loop() {
+  el.innerText += text[i]
+  i++
+
+  if (i == text.length) return
+  setTimeout(loop, Math.floor(Math.random() * 55) + 55)
 }
 
-gradient()
+loop()
