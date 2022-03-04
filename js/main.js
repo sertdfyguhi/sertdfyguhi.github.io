87757f491f91e4d54cf46c81dabf392f3b23ee40
sertdfyguhi-git <gdkamtalan@gmail.com>
2022-03-04 12:50:18 +0800

    huge update (i fucked up the last commit :pensive:)

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function type(el, text, min=55, max=110) {
+  let i = 0
+
+  function loop() {
+    el.textContent += text[i]
+    i++
+
+    if (i == text.length) return
+    setTimeout(loop, Math.floor(Math.random() * (max - min)) + min)
+  }
+
+  loop()
+}
+
+// random gradient bg
+const rgb1 = [
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256)
+]
+
+const rgb2 = [
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256),
+  Math.floor(Math.random() * 256)
+]
+
+document.body.style.background = `linear-gradient(90deg, rgb(${rgb1.join(', ')}), rgb(${rgb2.join(', ')}))`
