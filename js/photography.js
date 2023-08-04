39ba37c45ed1c79dd0b2f44ad86b31a18dbeda99
sertdfyguhi-git <gdkamtalan@gmail.com>
2023-08-04 22:22:41 +0800

    add slideshow gallery to photography page

---
+++ js/photography.js
@@ -0,0 +1,23 @@
+const slideshow = document.getElementById('slideshow');
+const slideshow_pos = slideshow.getBoundingClientRect().left;
+
+const images = slideshow.children;
+let shown = 0;
+
+document.getElementById('next-btn').addEventListener('click', e => {
+  shown = (shown + 1) % images.length;
+
+  slideshow.scrollBy({
+    left: images[shown].getBoundingClientRect().left - slideshow_pos,
+    behavior: 'smooth',
+  });
+});
+
+document.getElementById('prev-btn').addEventListener('click', e => {
+  shown = shown > 0 ? shown - 1 : images.length - 1;
+
+  slideshow.scrollBy({
+    left: images[shown].getBoundingClientRect().left - slideshow_pos,
+    behavior: 'smooth',
+  });
+});
